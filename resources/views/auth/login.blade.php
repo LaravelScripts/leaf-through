7e0c8dd1f6fb6fb013cd9ec8f8a5a002e233467a
vagrant <vagrant@homestead>
2016-11-23 19:02:30 +0000

    Completed Responsive Design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -76,7 +76,7 @@
                             Login
                         </button>
 
-                        <a class="btn btn-link" href="{{ url('/password/reset') }}">
+                        <a class="btn btn-link forgot-password" href="{{ url('/password/reset') }}">
                             Forgot Your Password?
                         </a>
                     </div>
