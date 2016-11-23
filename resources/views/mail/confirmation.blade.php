25871faaa9a37abf8cbdca59427632c5c66f538a
Vinay Kumar <vinaykumar1989@gmail.com>
2016-11-24 00:36:13 +0530

    Altered email link

--- resources/views/mail/confirmation.blade.php
+++ resources/views/mail/confirmation.blade.php
@@ -1,2 +1,2 @@
 <p>Hi {{ $user->name }},</p>
-<p>Go to the link {{ url('confirmation/'.$user->confirmation_hash) }} to confirm the account.</p>
+<p>Go to the link {{ url('confirmation/?email='.$user->email.'&code='.$user->confirmation_hash) }} to confirm the account.</p>
