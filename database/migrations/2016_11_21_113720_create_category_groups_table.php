8e3ebb08eba1e2b28e35c0963db10e169ca08641
Vinay Kumar <vinaykumar1989@gmail.com>
2016-11-25 17:50:20 +0530

    Alterations

--- database/migrations/2016_11_21_113720_create_category_groups_table.php
+++ database/migrations/2016_11_21_113720_create_category_groups_table.php
@@ -17,8 +17,7 @@ class CreateCategoryGroupsTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->integer('inbox_id')->unsigned();
-            $table->string('name', 100);
+            $table->string('name',100);
             $table->timestamps();
         });
     }
