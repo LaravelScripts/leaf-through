b26d0639f20a57b53996397cd8c003121ccb6427
Vinay Kumar <vinaykumar1989@gmail.com>
2016-11-25 17:47:56 +0530

    Relation model

--- app/CategoryGroup.php
+++ app/CategoryGroup.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryGroup extends Model
 {
     //
+    public function categorizedMail(){
+      return $this->hasMany('App\CategorizedMail','category_group_id');
+    }
 }
