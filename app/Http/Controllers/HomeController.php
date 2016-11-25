08bb900816f2e5d2416ba22fd08dd84c606a970d
Vinay Kumar <vinaykumar1989@gmail.com>
2016-11-25 17:49:20 +0530

    Pass inbox and categorygroups to view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,25 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Contracts\{InboxContract, CrudContract};
+use App\Traits\ReadabilityMode;
+
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+  use ReadabilityMode;
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('home');
-    }
+  private $inbox;
+  private $categoryGroups;
+  /**
+  * Create a new controller instance.
+  *
+  * @return void
+  */
+  public function __construct(InboxContract $inbox, CrudContract $categoryGroup)
+  {
+    //$this->middleware('auth');
+    $this->inbox = $inbox;
+    $this->categoryGroup = $categoryGroup;
+  }
+
+  /**
+  * Show the application dashboard.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index()
+  {
+    $this->readableFormat("https://laravel-news.com/2016/06/look-whats-coming-laravel-5-3/");
+    return view('home', ['inbox' => $this->inbox->show(), 'categoryGroups' => $this->categoryGroup->withCategorizedMail()]);
+  }
 }
