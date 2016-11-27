489ed55ad0afbb6655362162dff21e48bde81e03
Vinay Kumar <vinaykumar1989@gmail.com>
2016-11-28 00:03:35 +0530

    Register Notification event and listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,12 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\SomeEvent' => [
-            'App\Listeners\EventListener',
+        'App\Events\LinkShared' => [
+            'App\Listeners\SendLinkSharedEmail',
+        ],
+
+        'Illuminate\Notifications\Events\NotificationSent' => [
+          'App\Listeners\LogNotification',
         ],
     ];
 
