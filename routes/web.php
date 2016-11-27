c47516474ab5dadbfc89d05d483439dbbf779f41
Vinay Kumar <vinaykumar1989@gmail.com>
2016-11-28 00:06:36 +0530

    In progress..

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
-
 Route::get('/confirmation/{code}', 'Auth\RegisterController@confirm');
 Route::post('/sendconfirmationemail', 'Auth\RegisterController@resendConfirmation');
 
 Route::group(['middleware'=> 'auth'], function(){
-	Route::get('inbox', 'InboxController@messages');
+  Route::get('/home', 'HomeController@index');
+  Route::post('/emailsuggestion', 'ShareLink@recipientSuggestion');
+  Route::post('/share', 'ShareLink@send');
+  Route::any('/extracthtml', 'ShareLink@htmlcontents'); //Change it to appropiate later
+	//Route::get('/inbox', 'InboxController@messages');
 });
