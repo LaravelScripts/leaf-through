0fe0173f78fbcbb9a21f32d38bf581c9756b6721
vagrant <vagrant@homestead>
2016-11-28 02:28:56 +0000

    Commiting changes in settings controller

---
+++ app/Http/Controllers/SettingController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Setting;
+use Illuminate\Http\Request;
+
+class SettingController extends Controller
+{
+    public function getForm()
+    {
+    	return view('settings.form');
+    }
+
+    public function postForm(Request $request)
+    {
+    	$settings = getAllSettings();
+
+    	$rules = collect($settings)->filter(function($value) {
+    		return $value->required;
+    	})->mapWithKeys(function($value) {
+    		return [$value->name => 'required'];
+    	})->toArray();
+
+    	$this->validate($request, $rules);
+
+    	collect($request->all())->each(function($value, $key) {
+    		$setting = Setting::where('name', $key)->first();
+    		$this->save($setting);
+    	});
+
+    	cache()->forget('settings');
+
+    	return back();
+    }
+
+    private function save($setting)
+    {
+    	if ($setting) {
+			$setting->value = $value;
+			$setting->save();
+		}	
+    }
+}
