aa630a6a7609a9f3861237aed888d3082ea0bc62
Vinay Kumar <vinaykumar1989@gmail.com>
2016-11-29 23:51:24 +0530

    Relationship with CategorizedArticle

--- app/CategoryGroup.php
+++ app/CategoryGroup.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryGroup extends Model
 {
     //
-    public function categorizedMail(){
-      return $this->hasMany('App\CategorizedMail','category_group_id');
+    public function categorizedArticle(){
+      return $this->hasMany('App\CategorizedArticle','category_group_id');
     }
 }
