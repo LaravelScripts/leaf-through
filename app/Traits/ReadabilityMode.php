91c76f76461b3f0457f455837f9def4e541e580c
Vinay Kumar <vinaykumar1989@gmail.com>
2016-12-07 16:16:50 +0530

    Made use of mercury api

--- app/Traits/ReadabilityMode.php
+++ app/Traits/ReadabilityMode.php
@@ -3,40 +3,27 @@ namespace App\Traits;
 
 use Readability\Readability;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 trait ReadabilityMode{
 
   /**
   *
   */
-  function readableFormat($url, $html): array{
-    if (function_exists('tidy_parse_string')) {
-        $tidy = tidy_parse_string($html, array(), 'UTF8');
-        $tidy->cleanRepair();
-        $html = $tidy->value;
-    }
+  function readableFormat($url): array{
+      $client = new \GuzzleHttp\Client();
+      $res = $client->request('GET',
+                                'https://mercury.postlight.com/parser?url='.$url,
+                                [ 'headers' =>
+                                        [ 'User-Agent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11)',
+                                            'Content-Type' => 'application/json',
+                                            'x-api-key' => 'HPLqb1Emtpz7DpVkDOzXGRCOi4LkwKszEcPNwoVB'
+                                        ]
+                                    ]
+                                );
 
-    $readability = new Readability($html, $url);
-    // print debug output?
-    // useful to compare against Arc90's original JS version -
-    // simply click the bookmarklet with FireBug's console window open
-    $readability->debug = false;
-    // convert links to footnotes?
-    $readability->convertLinksToFootnotes = true;
-    // process it
-    $result = $readability->init();
-    // does it look like we found what we wanted?
-    if ($result) {
-        $content = $readability->getContent()->innerHTML;
-        // if we've got Tidy, let's clean it up for output
-        if (function_exists('tidy_parse_string')) {
-            $tidy = tidy_parse_string($content, array('indent'=>true, 'show-body-only' => true), 'UTF8');
-            $tidy->cleanRepair();
-            $content = $tidy->value;
-        }
-        return ["title" => htmlentities($readability->getTitle()->textContent, ENT_QUOTES), "content" => htmlentities($content, ENT_QUOTES)];
-    } else {
-        return ["error" => "Unable to fetch contents."];
-    }
+    return json_decode($res->getBody(), true);
   }
 
 }
